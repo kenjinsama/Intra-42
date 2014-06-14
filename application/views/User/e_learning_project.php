4c8b8f2640ab38d51d2ab633b1b1d24b5d851bfd
Sousa Dany <danysousa.sd@gmail.com>
2014-06-14 04:18:41 +0200

    ~ correction titre video

--- application/views/User/e_learning_project.php
+++ application/views/User/e_learning_project.php
@@ -3,7 +3,7 @@ E-Learning<BR />
 $i = 0;
 while (isset($project[$i])) :
 ?>
-<DIV class="e-learning-title"><?PHP $tmp = substr(strrchr($project[$i]->path, '/'), 1);echo substr($tmp, 0, strlen(strrchr($tmp, '.')) - 1);?></DIV>
+<DIV class="e-learning-title"><?PHP $tmp = substr(strrchr($project[$i]->path, '/'), 1);echo substr($tmp, 0, strlen($tmp) - strlen(strrchr($tmp, '.')));?></DIV>
 <video width="400" height="222" controls="controls">
 	<source src="<?PHP echo base_url().$project[$i]->path; ?>" type="video/mp4" />
 </video>

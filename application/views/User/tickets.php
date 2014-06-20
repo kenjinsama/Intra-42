7f87910a5013eddff69baf3de3fd6185ee175f33
Sousa Dany <danysousa.sd@gmail.com>
2014-06-20 12:38:35 +0200

    + resolve bug

--- application/views/User/tickets.php
+++ application/views/User/tickets.php
@@ -1,111 +1,113 @@
-<?php echo anchor(base_url() . "user/create_tickets", "New ticket", ["class" => "new_ticket_button"]);
-if (isset($tickets))
-{
-	echo heading("Mes tickets", 4);
-	?>
-
-<table>
-	<thead>
-		<tr>
-			<th>#</th>
-			<th>Priority</th>
-			<th>Title</th>
-			<th>Queue</th>
-			<th>Status</th>
-			<th>Author</th>
-			<th>Assigned</th>
-		</tr>
-	</thead>
-	<tbody>
-		<?php
-			foreach ($tickets as $ticket): ?>
-			<tr>
-				<td><?php echo anchor(base_url() . 'user/response-tickets/' . $ticket['id'], '['.$ticket['type'].'-'.$ticket['id'].']'); ?></td>
-				<td><?php echo $ticket['priority']; ?></td>
-				<td><?php echo anchor(base_url() . 'user/response-tickets/' . $ticket['id'], $ticket['title']); ?></a></td>
-				<td><?php echo $ticket['type']; ?></td>
-				<td><?php echo $ticket['state'] ?></td>
-				<td><?php echo  $this->check_log->obtain_name($ticket['id_user']) ?></td>
-				<td><?php echo ($ticket['id_admin'] == 0 ? "Mr Nobody" : $this->check_log->obtain_name($ticket['id_admin'])) ?></td>
-			</tr>
-		<?php
-			endforeach;
-}
+<div class="content_page">
+	<?php echo anchor(base_url() . "user/create_tickets", "New ticket", ["class" => "new_ticket_button"]);
+	if (isset($tickets))
+	{
+		echo heading("Mes tickets", 4);
 		?>
-	</tbody>
-</table>
 
-<?php
-if (isset($assign))
-{
-	echo heading("Les tickets qui me sont assigné", 4);
-	?>
-
-<table>
-	<thead>
-		<tr>
-			<th>#</th>
-			<th>Priority</th>
-			<th>Title</th>
-			<th>Queue</th>
-			<th>Status</th>
-			<th>Author</th>
-			<th>Assigned</th>
-		</tr>
-	</thead>
-	<tbody>
-		<?php
-			foreach ($assign as $ticket): ?>
+	<table>
+		<thead>
 			<tr>
-				<td><?php echo anchor(base_url() . 'user/response-tickets/'. $ticket['id'], '['.$ticket['type'].'-'.$ticket['id'].']'); ?></td>
-				<td><?php echo $ticket['priority']; ?></td>
-				<td><?php echo anchor(base_url() . 'user/response-tickets/'. $ticket['id'], $ticket['title']); ?></a></td>
-				<td><?php echo $ticket['type']; ?></td>
-				<td><?php echo $ticket['state'] ?></td>
-				<td><?php echo  $this->check_log->obtain_name($ticket['id_user']) ?></td>
-				<td><?php echo ($ticket['id_admin'] == 0 ? "Mr Nobody" : $this->check_log->obtain_name($ticket['id_admin'])) ?></td>
+				<th>#</th>
+				<th>Priority</th>
+				<th>Title</th>
+				<th>Queue</th>
+				<th>Status</th>
+				<th>Author</th>
+				<th>Assigned</th>
 			</tr>
-		<?php
-			endforeach;
-}
+		</thead>
+		<tbody>
+			<?php
+				foreach ($tickets as $ticket): ?>
+				<tr>
+					<td><?php echo anchor(base_url() . 'user/response-tickets/' . $ticket['id'], '['.$ticket['type'].'-'.$ticket['id'].']'); ?></td>
+					<td><?php echo $ticket['priority']; ?></td>
+					<td><?php echo anchor(base_url() . 'user/response-tickets/' . $ticket['id'], $ticket['title']); ?></a></td>
+					<td><?php echo $ticket['type']; ?></td>
+					<td><?php echo $ticket['state'] ?></td>
+					<td><?php echo  $this->check_log->obtain_name($ticket['id_user']) ?></td>
+					<td><?php echo ($ticket['id_admin'] == 0 ? "Mr Nobody" : $this->check_log->obtain_name($ticket['id_admin'])) ?></td>
+				</tr>
+			<?php
+				endforeach;
+	}
+			?>
+		</tbody>
+	</table>
+
+	<?php
+	if (isset($assign))
+	{
+		echo heading("Les tickets qui me sont assigné", 4);
 		?>
-	</tbody>
-</table>
 
-
-<?php
-if (isset($no_assign))
-{
-	echo heading("Les tickets du pool", 4);
-	?>
-
-<table>
-	<thead>
-		<tr>
-			<th>#</th>
-			<th>Priority</th>
-			<th>Title</th>
-			<th>Queue</th>
-			<th>Status</th>
-			<th>Author</th>
-			<th>Assigned</th>
-		</tr>
-	</thead>
-	<tbody>
-		<?php
-			foreach ($no_assign as $ticket): ?>
+	<table>
+		<thead>
 			<tr>
-				<td><?php echo anchor(base_url() . 'user/response-tickets/' . $ticket['id'], '['.$ticket['type'].'-'.$ticket['id'].']'); ?></td>
-				<td><?php echo $ticket['priority']; ?></td>
-				<td><?php echo anchor(base_url() . 'user/response-tickets/' . $ticket['id'], $ticket['title']); ?></a></td>
-				<td><?php echo $ticket['type']; ?></td>
-				<td><?php echo $ticket['state'] ?></td>
-				<td><?php echo  $this->check_log->obtain_name($ticket['id_user']) ?></td>
-				<td><?php echo ($ticket['id_admin'] == 0 ? "Mr Nobody" : $this->check_log->obtain_name($ticket['id_admin'])) ?></td>
+				<th>#</th>
+				<th>Priority</th>
+				<th>Title</th>
+				<th>Queue</th>
+				<th>Status</th>
+				<th>Author</th>
+				<th>Assigned</th>
 			</tr>
-		<?php
-			endforeach;
-}
+		</thead>
+		<tbody>
+			<?php
+				foreach ($assign as $ticket): ?>
+				<tr>
+					<td><?php echo anchor(base_url() . 'user/response-tickets/'. $ticket['id'], '['.$ticket['type'].'-'.$ticket['id'].']'); ?></td>
+					<td><?php echo $ticket['priority']; ?></td>
+					<td><?php echo anchor(base_url() . 'user/response-tickets/'. $ticket['id'], $ticket['title']); ?></a></td>
+					<td><?php echo $ticket['type']; ?></td>
+					<td><?php echo $ticket['state'] ?></td>
+					<td><?php echo  $this->check_log->obtain_name($ticket['id_user']) ?></td>
+					<td><?php echo ($ticket['id_admin'] == 0 ? "Mr Nobody" : $this->check_log->obtain_name($ticket['id_admin'])) ?></td>
+				</tr>
+			<?php
+				endforeach;
+	}
+			?>
+		</tbody>
+	</table>
+
+
+	<?php
+	if (isset($no_assign))
+	{
+		echo heading("Les tickets du pool", 4);
 		?>
-	</tbody>
-</table>
+
+	<table>
+		<thead>
+			<tr>
+				<th>#</th>
+				<th>Priority</th>
+				<th>Title</th>
+				<th>Queue</th>
+				<th>Status</th>
+				<th>Author</th>
+				<th>Assigned</th>
+			</tr>
+		</thead>
+		<tbody>
+			<?php
+				foreach ($no_assign as $ticket): ?>
+				<tr>
+					<td><?php echo anchor(base_url() . 'user/response-tickets/' . $ticket['id'], '['.$ticket['type'].'-'.$ticket['id'].']'); ?></td>
+					<td><?php echo $ticket['priority']; ?></td>
+					<td><?php echo anchor(base_url() . 'user/response-tickets/' . $ticket['id'], $ticket['title']); ?></a></td>
+					<td><?php echo $ticket['type']; ?></td>
+					<td><?php echo $ticket['state'] ?></td>
+					<td><?php echo  $this->check_log->obtain_name($ticket['id_user']) ?></td>
+					<td><?php echo ($ticket['id_admin'] == 0 ? "Mr Nobody" : $this->check_log->obtain_name($ticket['id_admin'])) ?></td>
+				</tr>
+			<?php
+				endforeach;
+	}
+			?>
+		</tbody>
+	</table>
+</div>
